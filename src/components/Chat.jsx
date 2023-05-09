082a1e7cee4857d5b62da52bac4b3051b781d25c
Harshit shukla <harshit shukla>
2023-05-09 15:26:37 +0530

    final

---
+++ src/components/Chat.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from "react";
+import Cam from "../img/cam.png";
+import call from "../img/call.png";
+// import Add from "../img/add.png";
+import More from "../img/more.png";
+import Messages from "./Messages";
+import Input from "./Input";
+import { ChatContext } from "../context/ChatContext";
+
+const Chat = () => {
+  const { data } = useContext(ChatContext);
+
+  return (
+    <div className="chat">
+      <div className="chatInfo">
+        <span>{data.user?.displayName}</span>
+        <div className="chatIcons">
+          <img src={call} alt="" />
+          <img src={Cam} alt="" />
+          <img src={More} alt="" />
+        </div>
+      </div>
+      <Messages />
+      <Input />
+    </div>
+  );
+};
+
+export default Chat;
